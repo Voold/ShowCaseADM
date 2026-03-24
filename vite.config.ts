87e9d10033bee6cbfcc9d4d56a855d81c47dc423
1eshiy <1eshiy@vk.com>
2026-03-25 00:06:57 +0700

    add vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        dimensions: false, // отключает добавление width и height
+        icon: true,
+        ref: true
+      },
+      include: '**/*.svg?react'
+    })
+  ]
 })
