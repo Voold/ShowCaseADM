dee1e41b0be0eb24bd4c111faee4e1c0dbd872a5
Mys2018 <kirillcodminecraft@gmail.com>
2026-03-24 16:51:38 +0700

    fix(styles): Исправление ошибок
    - Возвращена ссылка для стилей
    - Добавлена минимальная высота для Уведомлений
    - Удалены лишние цвета из файлов
    - Нет дубликата кода со шрифтами
    - Изменена логика фона для Уведомления
    - Вынесены типы пропсов Уведомлений

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,14 +1,5 @@
 import styles from './Toast.module.css'
-
-interface ToastProps {
-  status?: boolean,
-  title?: string,
-  description?: string,
-  link?: {
-    title_link: string,
-    link: string},
-  onClose?: () => void
-}
+import type {ToastProps} from "@/types/toastTypes.ts";
 
 const Toast = ({
   status = true, 
@@ -20,7 +11,7 @@ const Toast = ({
       <div className={`${styles.toast} ${status ? styles.green : styles.red}`}>
         <h5>{title}</h5>
         <p>{description}</p>
-        {link && <a href={link.link}>{link.title_link}</a>}
+        {link && <a href={link.link} target="_blank" rel="noopener noreferrer">{link.title_link}</a>}
       </div>
   )
 }
