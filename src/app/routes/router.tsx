dbe6a4dbd6f68d2068c8411093d5162984e0172b
1eshiy <1eshiy@vk.com>
2026-04-16 19:56:32 +0700

    set up base routing

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,26 +1,28 @@
-import {ProtectedRoute} from "@/features/protected-route";
-import {LoginPage} from "@/pages/login";
-import {MainPage} from "@/pages/main/";
-import {NotFoundPage} from "@/pages/not-found";
-import { ROUTES } from "@/shared/config";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from 'react-router-dom'
+import { LoginPage } from '@/pages/login'
+import { NotFoundPage } from '@/pages/not-found'
+import { MainPage } from '@/pages/main/'
+import { ProjectsPage } from '@/pages/projects'
+import { UsersPage } from '@/pages/users'
+import { RolesPage } from '@/pages/roles'
+import { ReportsPage } from '@/pages/reports'
+import { SettingsPage } from '@/pages/settings/ui/SettingsPage'
+import { ProtectedRoute } from '@/features/protected-route'
+import { ROUTES } from '@/shared'
 
 export const router = createBrowserRouter([
+  { path: '/', element: <Navigate to={ROUTES.ADMIN} replace /> },
+  { path: ROUTES.LOGIN, element: <LoginPage /> },
   {
-    path: ROUTES.LOGIN,
-    element: <LoginPage/>
-  },
-  {
-    path: "*",
-    element: <NotFoundPage/>
-  },
-  {
-    element: <ProtectedRoute/>,
+    element: <ProtectedRoute />,
     children: [
-      {
-        path: ROUTES.ADMIN,
-        element: <MainPage/>
-      }
+      { path: ROUTES.ADMIN, element: <MainPage /> },
+      { path: ROUTES.PROJECTS, element: <ProjectsPage /> },
+      { path: ROUTES.USERS, element: <UsersPage /> },
+      { path: ROUTES.ROLES, element: <RolesPage /> },
+      { path: ROUTES.REPORTS, element: <ReportsPage /> },
+      { path: ROUTES.SETTINGS, element: <SettingsPage /> }
     ]
-  }
+  },
+  { path: '*', element: <NotFoundPage /> }
 ])
