dbd3ec6f5ae6000821c33995e697b17f376025e8
1eshiy <1eshiy@vk.com>
2026-04-16 19:57:47 +0700

    inject routes in side bar

--- src/widgets/side-bar/ui/SideBar/SideBar.tsx
+++ src/widgets/side-bar/ui/SideBar/SideBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import DiagramIcon from '../../assets/diagram.svg?react'
 import FolderIcon from '../../assets/folder.svg?react'
 import GearIcon from '../../assets/gear.svg?react'
@@ -8,19 +9,20 @@ import UsersIcon from '../../assets/users.svg?react'
 import styles from './SideBar.module.css'
 import { NavButton } from '../NavButton/NavButton'
 import { UserSlot } from '@/entities/user'
-import { Logo } from '@/shared/ui'
+import { Logo, ROUTES } from '@/shared'
 
 const buttons = [
-  { name: 'Главная', icon: LayoutIcon, to: '' },
-  { name: 'Проекты', icon: FolderIcon, to: '' },
-  { name: 'Пользователи', icon: UsersIcon, to: '' },
-  { name: 'Модераторы', icon: HammerIcon, to: '' },
-  { name: 'Отчёты', icon: DiagramIcon, to: '' },
-  { name: 'Настройки', icon: GearIcon, to: '' }
+  { name: 'Главная', icon: LayoutIcon, to: ROUTES.ADMIN },
+  { name: 'Проекты', icon: FolderIcon, to: ROUTES.PROJECTS },
+  { name: 'Пользователи', icon: UsersIcon, to: ROUTES.USERS },
+  { name: 'Роли', icon: HammerIcon, to: ROUTES.ROLES },
+  { name: 'Отчёты', icon: DiagramIcon, to: ROUTES.REPORTS },
+  { name: 'Настройки', icon: GearIcon, to: ROUTES.SETTINGS }
 ]
 
 export function SideBar() {
-  const [activeButton, setActiveButton] = useState('Главная')
+  const location = useLocation()
+  const [activeButton, setActiveButton] = useState(location.pathname)
 
   return (
     <div className={styles.sideBar}>
@@ -32,8 +34,8 @@ export function SideBar() {
             to={button.to}
             key={button.name}
             Icon={button.icon}
-            isActive={activeButton === button.name}
-            onClick={() => setActiveButton(button.name)}
+            isActive={activeButton === button.to}
+            onClick={() => setActiveButton(button.to)}
           >
             {button.name}
           </NavButton>
