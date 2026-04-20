06effbfb8df631a0b8209455595ff046c1f0faf2
Voold <barsik.koteevich@mail.ru>
2026-04-21 00:53:42 +0700

    TanStack QUery devTools

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/shared";
 import { AuthBootstrapper } from "./auth-bootstrap/AuthBootstrapper";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface AppProviderProps {
   children?: ReactNode;
@@ -12,6 +13,7 @@ export const AppProvider = ({ children }: AppProviderProps) => {
     <QueryClientProvider client={queryClient}>
       <AuthBootstrapper />
       {children}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 };
