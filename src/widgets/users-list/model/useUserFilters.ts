cdd733c52cb661504b6f5c06018eb03fe2c23f66
1eshiy <1eshiy@vk.com>
2026-05-08 16:06:43 +0700

    add useUserFilters

---
+++ src/widgets/users-list/model/useUserFilters.ts
@@ -0,0 +1,39 @@
+import { useCallback } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+interface Filters {
+  query?: string
+  page?: number
+  limit?: number
+}
+
+export const useUserFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const query = searchParams.get('query') || ''
+  const page = Number(searchParams.get('page')) || 1
+  const limit = Number(searchParams.get('limit')) || 10
+
+  const updateFilters = useCallback(
+    (fields: Filters) => {
+      setSearchParams(prevParams => {
+        const newParams = new URLSearchParams(prevParams)
+        Object.entries(fields).forEach(([key, value]) => {
+          if (value === undefined || value === '') {
+            newParams.delete(key)
+          } else {
+            newParams.set(key, String(value))
+          }
+        })
+        return newParams
+      })
+    },
+    [setSearchParams]
+  )
+
+  const setQuery = useCallback((value: string) => updateFilters({ query: value, page: 1 }), [updateFilters])
+  const setPage = useCallback((page: number) => updateFilters({ page }), [updateFilters])
+  const setLimit = useCallback((limit: number) => updateFilters({ limit, page: 1 }), [updateFilters])
+
+  return { query, limit, page, offset: (page - 1) * limit, setQuery, setPage, setLimit }
+}
