cdee19752c64b25442df84166863fbc648707dda
1eshiy <1eshiy@vk.com>
2026-05-20 13:02:20 +0700

    fix user slot width related problem

--- src/entities/user/ui/UserSlot/UserSlot.tsx
+++ src/entities/user/ui/UserSlot/UserSlot.tsx
@@ -10,11 +10,11 @@ export interface UserSlotProps extends ComponentPropsWithRef<'div'> {
 export const UserSlot = ({ user, className, children, onClick, ...props }: UserSlotProps) => {
   return (
     <div className={`${styles.container} ${onClick && styles.clickable} ${className}`} onClick={onClick} {...props}>
-      <div className={styles.avatarContainer}>
+      <div className={styles.mainInfo}>
         {/* <img className={styles.avatar} src={user.profilePicture} alt="Фото профиля" /> */}
         <img className={styles.avatar} src={defaultProfilePicture} alt='Фото профиля' />{' '} {/* TODO убрать когда дадут аватарку на Base */}
         <div className={styles.description}>
-          <p className={styles.name}>{user.meta.name}</p>
+          <p title={user.meta.name} className={styles.name}>{user.meta.name}</p>
           <p className={styles.id}>{user.id}</p>
         </div>
       </div>
