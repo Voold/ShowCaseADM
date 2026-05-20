7c4548642945e16ed7dc9eee0fe8be478ad8324d
1eshiy <1eshiy@vk.com>
2026-05-20 12:15:34 +0700

    clickable user slot styles

--- src/entities/user/ui/UserSlot/UserSlot.tsx
+++ src/entities/user/ui/UserSlot/UserSlot.tsx
@@ -7,13 +7,12 @@ export interface UserSlotProps extends ComponentPropsWithRef<'div'> {
   user: UserBase
 }
 
-export const UserSlot = ({ user, className, children, ...props }: UserSlotProps) => {
+export const UserSlot = ({ user, className, children, onClick, ...props }: UserSlotProps) => {
   return (
-    <div className={`${styles.container} ${className}`} {...props}>
+    <div className={`${styles.container} ${onClick && styles.clickable} ${className}`} onClick={onClick} {...props}>
       <div className={styles.avatarContainer}>
         {/* <img className={styles.avatar} src={user.profilePicture} alt="Фото профиля" /> */}
-        <img className={styles.avatar} src={defaultProfilePicture} alt='Фото профиля' /> {/* TODO убрать когда дадут аватарку на Base */}
-
+        <img className={styles.avatar} src={defaultProfilePicture} alt='Фото профиля' />{' '} {/* TODO убрать когда дадут аватарку на Base */}
         <div className={styles.description}>
           <p className={styles.name}>{user.meta.name}</p>
           <p className={styles.id}>{user.id}</p>
