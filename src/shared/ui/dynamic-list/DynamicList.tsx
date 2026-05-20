c84d2fd08183905bff85dad3768a0e5069485589
1eshiy <1eshiy@vk.com>
2026-05-20 13:02:53 +0700

    hide pagination when single page

--- src/shared/ui/dynamic-list/DynamicList.tsx
+++ src/shared/ui/dynamic-list/DynamicList.tsx
@@ -1,47 +1,39 @@
-import {type ReactNode} from "react";
-import { Pagination } from "../pagination/Pagination.tsx";
-import { SearchInput } from "../search-input/SearchInput.tsx";
-import styles from './DynamicList.module.css';
+import { type ReactNode } from 'react'
+import { Pagination } from '../pagination/Pagination.tsx'
+import { SearchInput } from '../search-input/SearchInput.tsx'
+import styles from './DynamicList.module.css'
 
 interface DynamicListProps {
   // Нужный список
-  children: ReactNode,
+  children?: ReactNode
   // Для пагинации
-  currentPage: number,
-  totalPages: number,
-  setCurrentPage: (number: number) => void,
+  currentPage: number
+  totalPages: number
+  setCurrentPage: (number: number) => void
   // Для поиска
-  searchQuery: string,
-  setSearchQuery: (value: string) => void,
-  placeholder: string,
+  searchQuery: string
+  setSearchQuery: (value: string) => void
+  placeholder: string
 }
 
 export function DynamicList({
-    children,
-    currentPage,
-    totalPages,
-    setCurrentPage,
-    searchQuery,
-    setSearchQuery,
-    placeholder,
+  children,
+  currentPage,
+  totalPages,
+  setCurrentPage,
+  searchQuery,
+  setSearchQuery,
+  placeholder
 }: DynamicListProps) {
   return (
-      <div className={styles.container}>
-        <SearchInput
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            placeholder={placeholder}
-        />
-        <div className={styles.listWrapper}>
-          <div className={styles.list}>
-            {children}
-          </div>
-        </div>
-        <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPageSelect={setCurrentPage}
-        />
+    <div className={styles.container}>
+      <SearchInput value={searchQuery} onChange={e => setSearchQuery(e.target.value)} placeholder={placeholder} />
+      <div className={styles.listWrapper}>
+        <div className={styles.list}>{children}</div>
       </div>
-  );
+      {totalPages !== 1 && (
+        <Pagination currentPage={currentPage} totalPages={totalPages} onPageSelect={setCurrentPage} />
+      )}
+    </div>
+  )
 }
