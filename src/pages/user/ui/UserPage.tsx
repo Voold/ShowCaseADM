412dedab32e9d3147a67e155f41afae8b7bd1c97
1eshiy <1eshiy@vk.com>
2026-06-01 20:28:40 +0700

    use user projects list in user page

--- src/pages/user/ui/UserPage.tsx
+++ src/pages/user/ui/UserPage.tsx
@@ -1,30 +1,23 @@
 import { useParams } from 'react-router-dom'
 import styles from './UserPage.module.css'
-import { ProjectsList } from '@/widgets/projects-list'
+import { UserProjectsList } from '@/widgets/user-projects-list'
 import { IssuingARole } from '@/features/issuing-a-role'
-import {useUserById, UserSlot } from '@/entities/user'
+import { useUserById, UserSlot } from '@/entities/user'
 
 export const UserPage = () => {
-
-  const query = useParams()
-  const uid = query.id || ''
-
-  const { data, isLoading, error } = useUserById(uid)
+  const { id } = useParams()
+  const { data, isLoading, error } = useUserById(id)
 
   if (isLoading) return <h1>Загрузка</h1>
-  if (error || !data) return <h1>Ошибка</h1>
+  if (error || !data || !id) return <h1>Ошибка</h1>
 
   return (
     <main className={styles.mainContainer}>
       <h2>Страница пользователя</h2>
       <div className={styles.firstContainer}>
         <div className={styles.mainInfo}>
-          <div className={styles.userSlot}>
-            {data && <UserSlot user={data} />}
-          </div>
-          <IssuingARole
-            userRoles={data?.roles}
-          />
+          <div className={styles.userSlot}>{data && <UserSlot user={data} />}</div>
+          <IssuingARole userRoles={data?.roles} />
         </div>
         <div className={styles.container}>
           <div className={styles.secondInfo}>
@@ -32,13 +25,13 @@ export const UserPage = () => {
             {data?.email}
             <p className={styles.info}>Биография:</p>
             {data?.meta.bio ? data?.meta.bio : 'Нет'}
-            <p className={styles.info}>Скилы:</p>
+            <p className={styles.info}>Скиллы:</p>
             {data?.meta.skills ? data?.meta.skills : 'Нет'}
             <p className={styles.info}>Опыт:</p>
             {data?.meta.experience ? data?.meta.experience : 'Нет'}
           </div>
           <p className={styles.title}>Проекты пользователя</p>
-          <ProjectsList />
+          <UserProjectsList userId={id} />
         </div>
       </div>
     </main>
