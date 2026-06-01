cd6eb16f05e3c41c883a5d0f2cc064b20eceb240
1eshiy <1eshiy@vk.com>
2026-06-01 21:05:17 +0700

    remove projects search "enabled" property

--- src/entities/project/api/queries.ts
+++ src/entities/project/api/queries.ts
@@ -6,7 +6,6 @@ export const useProjectsByName = (query: string, offset: number, limit: number) 
   return useQuery({
     queryKey: queryKeys.search(query, offset, limit),
     queryFn: () => getProjectsByName(query, offset, limit),
-    enabled: !!query.trim(),
     staleTime: 60 * 1000 // 1 min
   })
 }
