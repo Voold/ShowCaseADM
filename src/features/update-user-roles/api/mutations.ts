7587d83490cbad23ab9f0413bf1da92a7beacdd8
1eshiy <1eshiy@vk.com>
2026-06-03 13:15:52 +0700

    add alert on role update error

--- src/features/update-user-roles/api/mutations.ts
+++ src/features/update-user-roles/api/mutations.ts
@@ -1,6 +1,16 @@
+import axios from 'axios'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import type { RemoveUserRoleVariables, SetUserRoleVariables } from './types'
-import { deleteUserRole, putUserRole, ROLE_WEIGHTS, userKeys, type User, type UserRole } from '@/entities/user'
+import {
+  deleteUserRole,
+  putUserRole,
+  ROLE_WEIGHTS,
+  ROLES_TRANSLATIONS,
+  userKeys,
+  type User,
+  type UserRole
+} from '@/entities/user'
+import { type BackendError } from '@/shared'
 
 export const useSetUserRole = () => {
   const queryClient = useQueryClient()
@@ -16,9 +26,13 @@ export const useSetUserRole = () => {
         queryClient.setQueryData<User>(queryKey, {
           ...previousUser,
           roles: [...previousUser.roles, { type, weight: ROLE_WEIGHTS[type], ...payload } as UserRole]
-          // вместо приведения типов ради безопасности можно использовать switch case c assertNever, но код сильно раздуется.
         })
       }
+    },
+    onError: (error, vars) => {
+      const isBackendError = axios.isAxiosError<BackendError>(error) && error.response
+      const message = isBackendError ? error.response?.data.msg : error.message
+      alert(`При добавлении роли ${ROLES_TRANSLATIONS[vars.type]} произошла ошибка\n${message}`)
     }
   })
 }
@@ -39,6 +53,11 @@ export const useRemoveUserRole = () => {
           roles: previousUser.roles.filter(role => role.type !== type)
         })
       }
+    },
+    onError: (error, vars) => {
+      const isBackendError = axios.isAxiosError<BackendError>(error) && error.response
+      const message = isBackendError ? error.response?.data.msg : error.message
+      alert(`При удалении роли ${ROLES_TRANSLATIONS[vars.type]} произошла ошибка\n${message}`)
     }
   })
 }
