cfc0cf8302a4c04f619c7d60147950b7478d3c56
1eshiy <1eshiy@vk.com>
2026-06-15 13:11:53 +0700

    add create tag feature

---
+++ src/features/create-tag/api/mutations.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createTag, queryKeys, type Tag } from '@/entities/tags'
+import { useToastsStore } from '@/entities/toast'
+import type { BackendError } from '@/shared'
+
+export const useCreateTag = () => {
+  const queryClient = useQueryClient()
+  const { show } = useToastsStore()
+
+  return useMutation({
+    mutationFn: (payload: Omit<Tag, 'id'>) => createTag(payload),
+    onSuccess: (tagId, vars) => {
+      const queryKey = queryKeys.tag(tagId)
+      queryClient.setQueryData<Tag>(queryKey, { id: tagId, ...vars })
+      show({ status: 'success', title: 'Успех', description: `Тег "${vars.name}" успешно добавлен` })
+    },
+    onError: (error, vars) => {
+      const isBackendError = axios.isAxiosError<BackendError>(error) && error.response
+      const message = isBackendError ? error.response?.data.msg : error.message
+      show({ status: 'error', title: 'Ошибка', description: `Произошла ошибка при создании тега "${vars.name}": ${message}"` })
+    }
+  })
+}
