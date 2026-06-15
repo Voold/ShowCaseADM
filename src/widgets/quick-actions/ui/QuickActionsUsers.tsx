6df39611ed46502546e6dc4a871d1e9831697e01
1eshiy <1eshiy@vk.com>
2026-06-15 19:31:56 +0700

    fixes

--- src/widgets/quick-actions/ui/QuickActionsUsers.tsx
+++ src/widgets/quick-actions/ui/QuickActionsUsers.tsx
@@ -5,16 +5,12 @@ import PlusIcon from '../assets/plus.svg?react'
 import { AgreeButton } from '@/shared'
 
 interface Users {
-  name: string,
+  name: string
   id: string
 }
 
-
 export const QuickActionsUsers = () => {
-  
-  const [users, setUsers] = useState<Users[]>(
-    []
-  )
+  const [users, setUsers] = useState<Users[]>([])
   const [value, setValue] = useState('')
 
   const addUser = (id: string) => {
@@ -35,46 +31,33 @@ export const QuickActionsUsers = () => {
   const handleAddUser = (id: string) => {
     const res = addUser(id)
     if (res.code === 201) {
-      setUsers((prev) => [...prev, {id, name: 'Костромской О. Е.'}])
+      setUsers(prev => [...prev, { id, name: 'Костромской О. Е.' }])
     } else {
       alert('Такого пользователя не существует')
     }
   }
 
-
-
   return (
-    <QuickActions title="Быстрые действия">
+    <QuickActions title='Быстрые действия'>
       <div className={styles.block}>
-
         <p className={styles.snippet}>Выдать роль &quot;Наставник&quot;</p>
 
         <div className={styles.inputBlock}>
-            <input
-                className={styles.input}
-                type='text'
-                placeholder='12345'
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-            />
-            <button className={styles.addButton} onClick={() => handleAddUser(value)}>
-              <PlusIcon/>
-            </button>
+          <input className={styles.input} type='text' placeholder='12345' value={value} onChange={e => setValue(e.target.value)} />
+          <button className={styles.addButton} onClick={() => handleAddUser(value)}>
+            <PlusIcon />
+          </button>
         </div>
 
         <div className={styles.namesContainer}>
-          {
-            users.map((user) => (
-              <div key={user.id} className={styles.user}>
-                {user.name} [{user.id}]
-              </div>
-            ))
-          }
+          {users.map(user => (
+            <div key={user.id} className={styles.user}>
+              {user.name} [{user.id}]
+            </div>
+          ))}
         </div>
 
-        <AgreeButton
-          active={users.length > 0 ? true : false}
-        />
+        <AgreeButton disabled={users.length === 0}>Подтвердить</AgreeButton>
       </div>
     </QuickActions>
   )
--- src/widgets/side-bar/ui/NavButton/NavButton.tsx
+++ src/widgets/side-bar/ui/NavButton/NavButton.tsx
@@ -4,13 +4,11 @@ import { NavLink, type LinkProps } from 'react-router-dom'
 interface NavButtonProps extends LinkProps {
   Icon?: React.FC<React.SVGProps<SVGSVGElement>>
   className?: string
-  isActive?: boolean
 }
 
-export function NavButton({ Icon, className, isActive = false, children, ...props }: NavButtonProps) {
+export function NavButton({ Icon, className, children, ...props }: NavButtonProps) {
   return (
-    // <NavLink className={({isActive}) => `${styles.button} ${isActive && styles.active} ${className}`} {...props}>
-    <NavLink className={`${styles.button} ${isActive && styles.active} ${className}`} {...props}>
+    <NavLink className={({isActive}) => `${styles.button} ${isActive && styles.active} ${className}`} {...props}>
       {Icon && <Icon className={styles.icon} />}
       {children}
     </NavLink>
