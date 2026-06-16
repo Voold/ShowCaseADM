f70af0993b532732127cc6d5506ea1314e98359a
1eshiy <1eshiy@vk.com>
2026-06-16 21:27:23 +0700

    query keys fix

--- src/entities/tags/api/queryKeys.ts
+++ src/entities/tags/api/queryKeys.ts
@@ -1,5 +1,5 @@
 export const queryKeys = {
-  all: 'tags' as const,
+  all: ['tags'] as const,
   tag: (tagId: string) => [...queryKeys.all, 'tag', tagId] as const,
   search: (query: string, offset: number, limit: number) => [...queryKeys.all, 'search', { query, limit, offset }] as const
 }
