2100c498b6d95b90b2752b71d767dbe703612672
1eshiy <1eshiy@vk.com>
2026-06-16 21:25:42 +0700

    add settings tabs widget

---
+++ src/widgets/settings-tabs/ui/SettingsTabs.tsx
@@ -0,0 +1,12 @@
+import { ROUTES, VerticalTabs, type TabProps } from '@/shared'
+
+const tabs: TabProps[] = [
+  { name: 'Теги', to: ROUTES.SETTINGS.TAGS },
+  // { name: 'Роли', to: ROUTES.SETTINGS.PROJECT_ROLES },
+  // { name: 'Партнёры', to: ROUTES.SETTINGS.PARTNERS },
+  // { name: 'Чекпоинты', to: ROUTES.SETTINGS.CHECKPOINTS }
+]
+
+export function SettingsTabs() {
+  return <VerticalTabs items={tabs} />
+}
