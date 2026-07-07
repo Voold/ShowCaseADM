e67befc2faa810ee7f636e8f997670e09cedb8e8
1eshiy <1eshiy@vk.com>
2026-07-07 17:14:39 +0700

    add tag-group model and api

--- src/entities/tag/api/queryKeys.ts
+++ src/entities/tag/api/queryKeys.ts
@@ -1,4 +1,6 @@
 export const queryKeys = {
   all: ['tags'] as const,
   tag: (tagId: string) => [...queryKeys.all, 'tag', tagId] as const,
+  groups: () => [...queryKeys.all, 'groups'] as const,
+  group: (groupId: string) => [...queryKeys.groups(), 'group', groupId] as const
 }
--- src/entities/tag/api/requests.ts
+++ src/entities/tag/api/requests.ts
@@ -7,10 +7,21 @@ export const getTags = async (): Promise<TagGroup[]> => {
   return data.map(mapTagGroupDto)
 }
 
+export const getTagGroups = async (): Promise<Omit<TagGroup, 'tags'>[]> => {
+  const { data } = await api.get<Omit<TagGroupDto, 'tags'>[]>(ENDPOINTS.TAG_GROUPS)
+  return data.map(dto => ({ id: dto.groupId, name: dto.groupName }))
+}
+
 export const createTag = async (payload: Omit<Tag, 'id'>): Promise<string> => {
   const dtoPayload: Omit<TagDto, 'tagId'> = { tagName: payload.name, groupId: payload.groupId }
-  const { data } = await api.post<{tagId: string}>(ENDPOINTS.TAGS, dtoPayload)
+  const { data } = await api.post<{ tagId: string }>(ENDPOINTS.TAGS, dtoPayload)
   return data.tagId
+}
+
+export const createTagGroup = async (payload: Omit<TagGroup, 'id' | 'tags'>): Promise<string> => {
+  const dtoPayload: Omit<TagGroupDto, 'groupId' | 'tags'> = { groupName: payload.name }
+  const { data } = await api.post<{ groupId: string }>(ENDPOINTS.TAG_GROUPS, dtoPayload)
+  return data.groupId
 }
 
 export const editTag = async (id: string, payload: Tag): Promise<void> => {
@@ -18,6 +29,15 @@ export const editTag = async (id: string, payload: Tag): Promise<void> => {
   await api.put(ENDPOINTS.TAG_BY_ID(id), dtoPayload)
 }
 
+export const editTagGroup = async (id: string, payload: Omit<TagGroup, 'tags'>): Promise<void> => {
+  const dtoPayload: Omit<TagGroupDto, 'tags'> = { groupId: payload.id, groupName: payload.name }
+  await api.put(ENDPOINTS.TAG_GROUP_BY_ID(id), dtoPayload)
+}
+
 export const removeTag = async (id: string): Promise<void> => {
   await api.delete(ENDPOINTS.TAG_BY_ID(id))
+}
+
+export const removeTagGroup = async (id: string): Promise<void> => {
+  await api.delete(ENDPOINTS.TAG_GROUP_BY_ID(id))
 }
