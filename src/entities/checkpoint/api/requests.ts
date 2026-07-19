4f15149597115d9e9ada4a7f68755c0d16db2589
1eshiy <1eshiy@vk.com>
2026-07-19 15:56:37 +0700

    checkpoints requests

---
+++ src/entities/checkpoint/api/requests.ts
@@ -0,0 +1,21 @@
+import { mapCheckpointGroupDto, mapCheckpointGroupToDto, mapCheckpointToDto } from '../lib/mappers'
+import type { Checkpoint, CheckpointGroup, CheckpointGroupDto } from '../model/types'
+import { api, ENDPOINTS } from '@/shared'
+
+export const getCheckpointGroups = async (): Promise<CheckpointGroup[]> => {
+  const { data } = await api.get<CheckpointGroupDto[]>(ENDPOINTS.CHECKPOINTS)
+  return data.map(mapCheckpointGroupDto)
+}
+
+export const createCheckpointGroup = async (payload: Checkpoint): Promise<string> => {
+  const { data } = await api.post<{ checkpointId: string }>(ENDPOINTS.CHECKPOINTS, mapCheckpointToDto(payload))
+  return data.checkpointId
+}
+
+export const editCheckpointGroup = async (payload: CheckpointGroup): Promise<void> => {
+  await api.put(ENDPOINTS.CHECKPOINT_BY_ID(payload.id), mapCheckpointGroupToDto(payload))
+}
+
+export const removeCheckpointGroup = async (checkpointId: string): Promise<void> => {
+  await api.delete(ENDPOINTS.CHECKPOINT_BY_ID(checkpointId))
+}
