b0ab2923d731069ea1fb3f5cfd8998f084fbd86e
1eshiy <1eshiy@vk.com>
2026-07-20 15:54:01 +0700

    make shared mapper better and add a similar one

--- src/shared/lib/mappers.ts
+++ src/shared/lib/mappers.ts
@@ -1,1 +1,13 @@
-export const mapDateToString = (date: Date) => `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+export const mapDateToBackendString = (date: Date) => {
+  const day = date.getDate().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const year = date.getFullYear().toString()
+  return `${year}-${month}-${day}`
+}
+
+export const mapDateToLocalString = (date: Date) => {
+  const day = date.getDate().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const year = date.getFullYear().toString()
+  return `${day}.${month}.${year}`
+}
