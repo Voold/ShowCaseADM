97c2b5f84d873b37b0f1a5a25b08506b63a756f3
1eshiy <1eshiy@vk.com>
2026-07-27 13:56:32 +0700

    new input prop

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -3,17 +3,28 @@ import styles from './Input.module.css'
 import { CloseButton } from '../close-button/CloseButton'
 
 interface InputProps extends ComponentPropsWithoutRef<'input'> {
-  leadingIcon?: ReactElement 
+  leadingIcon?: ReactElement
+  isCloseAlwaysVisible?: boolean
   onClear?: () => void
 }
 
-export function Input({ onClear, className, placeholder, value, onChange, children, leadingIcon, ...props }: InputProps) {
+export function Input({
+  onClear,
+  className,
+  placeholder,
+  value,
+  onChange,
+  children,
+  leadingIcon,
+  isCloseAlwaysVisible = false,
+  ...props
+}: InputProps) {
   return (
     <div className={`${styles.container} ${className ?? ''}`}>
       {leadingIcon}
       <input type='text' placeholder={placeholder} value={value} onChange={onChange} {...props} />
       {children}
-      {onClear && value && <CloseButton className={styles.closeButton} onClick={onClear} />}
+      {((onClear && value) || isCloseAlwaysVisible) && <CloseButton className={styles.closeButton} onClick={onClear} type='button' />}
     </div>
   )
 }
