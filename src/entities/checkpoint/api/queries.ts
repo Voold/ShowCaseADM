9f1320d804ccae4bf768e50aa7a8cc00a15c9f27
1eshiy <1eshiy@vk.com>
2026-07-31 15:36:40 +0700

    checkpoints api correction

--- src/entities/checkpoint/api/queries.ts
+++ src/entities/checkpoint/api/queries.ts
@@ -1,11 +1,11 @@
-import { useQuery } from "@tanstack/react-query"
-import { queryKeys } from "./queryKeys"
-import { getCheckpointGroups } from "./requests"
+import { useQuery } from '@tanstack/react-query'
+import { queryKeys } from './queryKeys'
+import { getCheckpointGroups } from './requests'
 
-export const useCheckpointGroups = () => {
-	return useQuery({
-		queryKey: queryKeys.all,
-		queryFn: getCheckpointGroups,
-		staleTime: 60 * 1000
-	})
+export const useCheckpointGroups = (limit: number = 10, offset: number = 0) => {
+  return useQuery({
+    queryKey: queryKeys.all,
+    queryFn: () => getCheckpointGroups(limit, offset),
+    staleTime: 60 * 1000
+  })
 }
--- src/entities/checkpoint/api/requests.ts
+++ src/entities/checkpoint/api/requests.ts
@@ -1,14 +1,22 @@
 import { mapCheckpointGroupDto, mapCheckpointGroupToDto, mapCheckpointToDto } from '../lib/mappers'
-import type { CheckpointDto, CheckpointGroup, CheckpointGroupDto } from '../model/types'
+import type { CheckpointDto, CheckpointGroup } from '../model/types'
+import type { GetCheckpointGroupsResponse } from './types'
 import { api, ENDPOINTS } from '@/shared'
 
-export const getCheckpointGroups = async (): Promise<CheckpointGroup[]> => {
-  const { data } = await api.get<CheckpointGroupDto[]>(ENDPOINTS.CHECKPOINTS)
-  return data.map(mapCheckpointGroupDto)
+export const getCheckpointGroups = async (
+  limit: number,
+  offset: number
+): Promise<{ checkpointGroups: CheckpointGroup[]; total: number }> => {
+  const params = { offset, limit }
+  const { data } = await api.get<GetCheckpointGroupsResponse>(ENDPOINTS.CHECKPOINTS, { params })
+  return { total: data.total, checkpointGroups: data.checkpoints.map(mapCheckpointGroupDto) }
 }
 
 export const createCheckpointGroup = async (payload: Omit<CheckpointGroup, 'id'>): Promise<string> => {
-  const payloadDto: { name: string; checkpoints: CheckpointDto[] } = { name: payload.title, checkpoints: payload.checkpoints.map(mapCheckpointToDto) }
+  const payloadDto: { name: string; checkpoints: CheckpointDto[] } = {
+    name: payload.title,
+    checkpoints: payload.checkpoints.map(mapCheckpointToDto)
+  }
   const { data } = await api.post<{ checkpointId: string }>(ENDPOINTS.CHECKPOINTS, payloadDto)
   return data.checkpointId
 }
