52bd73055a5a4ed1645885ed4522228b5a0f86fe
1eshiy <1eshiy@vk.com>
2026-08-06 11:51:13 +0700

    project queries

--- src/entities/project/index.ts
+++ src/entities/project/index.ts
@@ -1,3 +1,3 @@
-export { ProjectSlot } from "./ui/project-slot/ProjectSlot.tsx";
-export type { Project } from "./model/types.ts";
-export { useProjectsByName, useUserProjects } from "./api/queries.ts";
+export { ProjectSlot } from './ui/project-slot/ProjectSlot.tsx'
+export type { Project } from './model/types.ts'
+export { useProjects as useProjectsByName, useUserProjects } from './api/queries.ts'
